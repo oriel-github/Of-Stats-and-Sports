55f4b730847427cb0facdc7a12b8aa826da9ad37
Oriel <messageoriel@gmail.com>
2024-05-15 11:07:37 -0400

    Separated Population corr output and Pairwise Sport corr as separate classes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from analysis import SportCorrelations as Corr
+from analysis import SportCorrelations as Corrs
 
 ## Loads all wiki tables from URL page into list, so formatting assumes current wiki table column layout
 ## Specifically Metro name at col 0, population at 3, Big 4 at 5-8
@@ -11,29 +11,44 @@ def get_sport_list(formatted_population_data):
     return list(formatted_population_data.columns[1:])
 
 
-class PopulationPerformanceRelationship(Corr):
+class SportsPerformanceRelationship(Corrs):
 
-    ## Sport name in sports_list must be same format as it appears in population_data_filename data
-    ## e.g. if population data has 'N.H.L.' as column name, then its sport_name needs to be 'N.H.L.' 
     ## sports_list and sport_data_filename_list need to correspond, e.g. ['NHL', 'NBA'] requires ['nhl.csv', 'nba.csv'] order
     def __init__(self, sport_data_filename_list, population_data_filename, pop_data_table_no = 1):
-        self.population_data = get_population_data(population_data_filename, pop_data_table_no)
-        sports = get_sport_list(self.population_data)
-        super().__init__(self.population_data, sports, sport_data_filename_list)
+        population_data = get_population_data(population_data_filename, pop_data_table_no)
+        super().__init__(population_data, get_sport_list(population_data), sport_data_filename_list)
         self.get_pair_corrs()
         self.get_pair_corr_tests()
         self.get_pair_corr_pvals()
 
     def __str__(self):
         results = self.get_pval_matrix().map(lambda x: self.classify_pval(x))
-        return f'Pairwise Sport Performance Results:\n {results}'
+        return f'Pairwise Sport Performance t-Test Results:\n {results}'
     
-    def classify_pval(self, p_val_matrix_element):
-        if p_val_matrix_element < 0.05: return 'Different'
-        else: return 'Similar'
+    def classify_pval(self, p_val): ## Decision Threshold at 95% Confidence 
+        if p_val < 0.05: return 'Not Same'
+        else: return 'Same'
 
 
+class PopulationPerformanceRelationship(Corrs):
 
+    def __init__(self, sport_data_filename_list, population_data_filename, pop_data_table_no = 1):
+        self.population_data = get_population_data(population_data_filename, pop_data_table_no)
+        sports = get_sport_list(self.population_data)
+        super().__init__(self.population_data, sports, sport_data_filename_list)
+
+    def __str__(self):
+        results = {team:corr.coeff for team, corr in self.metro_corrs.items()}
+        df = pd.DataFrame.from_dict(results, orient='index', columns=['Pearson R Coefficient'])
+        df['Class'] = df['Pearson R Coefficient'].apply(self.classify_corr)
+        return f"Population-Performance Correlation Results:\n {df}"
+    
+    def classify_corr(self, corr):
+        if float('%.2f' % corr) == 0.00: return 'No Correlation'  ## Corr at most ±0.499%, basically insignificant corr to report
+        elif corr > 0.7: return 'Strong, Positive Correlation'
+        elif corr < -0.7: return 'Strong, Negative Correlation'
+        elif corr < 0: return 'Weak, Negative Correlation'
+        else: return 'Weak, Positive Correlation'
 
 
 if __name__ == "__main__":
